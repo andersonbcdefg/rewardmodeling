0faa180438478e23219e3ada880b6886060a6ad6
Benjamin Anderson <andersonbcdefg@gmail.com>
2023-06-05 15:07:44 -0700

    print batch sizes before commencing training

--- train.py
+++ train.py
@@ -78,9 +78,14 @@ def train(
 ):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
+    accumulation_steps = effective_batch_size // microbatch_size
+    if accelerator.is_main_process:
+        print(f"Effective batch size: {effective_batch_size}")
+        print(f"Microbatch size: {microbatch_size}")
+        print(f"Accumulation steps: {accumulation_steps}")
     accelerator = Accelerator(
         mixed_precision="bf16",
-        gradient_accumulation_steps=effective_batch_size // microbatch_size
+        gradient_accumulation_steps=accumulation_steps
     )
     train_dataloader = None
     eval_dataloaders = None
